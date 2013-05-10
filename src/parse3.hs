09d8ecec9af1c9d468f3a335a88235dde1163831
Matt Fenwick <mfenwick100@gmail.com>
2013-05-09 21:44:56 -0400

    make 3rd parser self-sufficient and implement the new, simple grammar

--- src/parse3.hs
+++ src/parse3.hs
@@ -1,66 +1,153 @@
 {-# LANGUAGE   NoMonomorphismRestriction
-             , FlexibleContexts           #-}
+             , FlexibleContexts       
+             , FlexibleInstances    
+             , MultiParamTypeClasses  #-}
 
-import Control.Monad.State       (MonadState (..), StateT(..), State)
-import Control.Monad.Error       (MonadError (..), ErrorT(..))
-import Control.Monad.Trans.Maybe (MaybeT(..))
+
+import Control.Monad             (liftM, ap)
+import Control.Monad.State       (MonadState (..), StateT(..))
+import Control.Monad.Error       (MonadError (..))
 import Control.Monad.Trans.Class (MonadTrans (..))
+import Control.Monad.Trans.Maybe (MaybeT(..))
 import Control.Applicative       (Applicative(..))
 import Data.List                 (nub)
-import Data.Functor.Identity     (Identity(..))
-import Parser
-import Classes
-import Instances
-import Common
 
-
--- other effects to try:
+-- should also try:
 --   partial results
---   whitespace/comment reporting
 
--- list of already seen forms,
--- list of text, line, column
---   for whitespace/comment
-type Log = [(String, Int, Int)]
+-- the prelude
+
+class Applicative f => Plus f where
+  zero :: f a
+  (<+>) :: f a -> f a -> f a
+
+many0 :: Plus f => f a -> f [a]
+many0 p = many1 p <+> pure []
+
+many1 :: Plus f => f a -> f [a]
+many1 p = pure (:) <*> p <*> many0 p
+
+class Switch f where
+  switch :: f a -> f ()
+
+data Either' a b
+    = Left' a
+    | Right' b
+  deriving (Show, Eq)
+
+  
+instance Plus Maybe where
+  zero = Nothing
+  Nothing <+> y = y
+  x       <+> _ = x
+
+instance (Functor m, Monad m) => Plus (MaybeT m) where
+  zero = MaybeT (return Nothing)
+  MaybeT l  <+>  MaybeT r  =  MaybeT x
+    where
+      x = l >>= \y -> case y of Nothing -> r;
+                                Just _  -> return y;
+
+instance (Monad m, Plus m) => Plus (StateT s m) where
+  zero = StateT (const zero)
+  StateT f <+> StateT g = StateT (\s -> f s <+> g s)
+
+
+instance Switch Maybe where
+  switch (Just _) = Nothing
+  switch Nothing  = Just ()
+
+instance (Functor m, Switch m) => Switch (StateT s m) where
+  switch (StateT f) = StateT (\s -> fmap (const ((), s)) . switch $ f s)
+
+instance Functor m => Switch (MaybeT m) where
+  switch (MaybeT m) = MaybeT (fmap switch m)
+
+
+instance Functor (Either' e) where
+  fmap = liftM
+
+instance Applicative (Either' e) where
+  pure = return
+  (<*>) = ap
+
+instance Monad (Either' e) where
+  return            =  Right'
+  Left' e   >>=  _  =  Left' e
+  Right' x  >>=  f  =  f x
+
+instance MonadError e (Either' e) where
+  throwError                =  Left'
+  catchError  (Right' x) _  =  Right' x
+  catchError  (Left' e)  f  =  f e
+
+
+data AST
+    = ANumber Integer
+    | ASymbol String
+    | AString String
+    | ALambda [String] [AST]
+    | ADefine String AST
+    | AApp    AST  [AST]
+  deriving (Show, Eq)
+
+
+type Token = (Char, Int, Int)
+chr  (a, _, _)  =  a
+line (_, b, _)  =  b
+col  (_, _, c)  =  c
+
+countLineCol :: [Char] -> [Token]
+countLineCol = reverse . snd . foldl f ((1, 1), [])
+  where
+    f ((l, c), ts) '\n'   = ((l + 1, 1), ('\n', l, c):ts)
+    f ((l, c), ts)  char  = ((l, c + 1), (char, l, c):ts)
+
+
+example = "{define \n\
+\  f \n\
+\  {lambda {x y}\n\
+\    (plus x y)}}\n\
+\\n\
+\(a b (c d e))\n\
+\\n\
+\; here's a nice comment !!\n\
+\\n\
+\"
+
+item :: (MonadState [t] m, Plus m) => m t
+item =
+    get >>= \xs -> case xs of
+                        (t:ts) -> put ts *> pure t;
+                        []     -> zero;
+
+check :: (Monad m, Plus m) => (a -> Bool) -> m a -> m a
+check f p =
+    p >>= \x ->
+    if (f x) then return x else zero
+
+satisfy :: (MonadState [t] m, Plus m) => (t -> Bool) -> m t
+satisfy = flip check item
+
+not1 :: (MonadState [t] m, Plus m, Switch m) => m a -> m t
+not1 p = switch p *> item
+
+end :: (MonadState [t] m, Plus m, Switch m) => m ()
+end = switch item
+
+commit :: (MonadError e m, Plus m) => e -> m a -> m a
+commit err p = p <+> throwError err
+
 
 logJunk text line col =
     lift get              >>= \junks ->
     lift $ put ((text, line, col):junks)
 
-type Error = (String, Token)
+-- the actual parser
+
 
 character :: (MonadState [Token] m, Plus m) => Char -> m Token
 character c = satisfy ((==) c . chr)
-
-ocurly = character '{'
-
-ccurly = character '}'
-
-oparen = character '('
-
-cparen = character ')'
-
-digit = fmap chr $ satisfy (flip elem ['0' .. '9'] . chr)
-
-number = munch $ many1 digit
-
-symbolOpens = ['a' .. 'z'] ++ ['A' .. 'Z'] ++ "!@#$%^&*_-+=:?<>/"
-
-symbol = munch (fmap (:) first <*> rest)
-  where
-    first = fmap chr $ satisfy (flip elem symbolOpens . chr)
-    rest = many0 (first <+> digit)
-    
-string = munch (dq          >>= \open ->
-                many0 char  <* 
-                commit ("unclosed string literal", open) dq)
-  where
-    dq = character '"'
-    char = fmap chr (escape <+> normal)
-    escape = character '\\' *> slash_or_dq
-    normal = not1 slash_or_dq
-    slash_or_dq = character '\\' <+> character '"'
-
 
 whitespace = 
     (many1 $ satisfy (flip elem " \n\t\r\f" . chr)) >>= \toks ->
@@ -73,81 +160,75 @@ whitespace =
 comment = 
     character ';'                     >>= \op ->
     many0 (not1 $ character '\n')     >>= \rest ->
---    return ()
     logJunk (map chr rest) (line op) (col op)
 
 munch p = many0 (whitespace <+> comment) *> p
 
-top = munch oparen
-tcp = munch cparen
-toc = munch ocurly
-tcc = munch ccurly
+ocurly = munch $ character '{'
+ccurly = munch $ character '}'
+oparen = munch $ character '('
+cparen = munch $ character ')'
+symbol = munch $ many1 char
+  where char = fmap chr $ satisfy (flip elem (['a' .. 'z'] ++ ['A' .. 'Z']) . chr)
 
--- tsymbol :: (MonadState [Token] m, Plus m, Switch m) => m AST
-tsymbol = fmap ASymbol symbol
+eaOp   =  "application: missing operator"
+eaCls  =  "application: missing close parenthesis"
+edSym  =  "define: missing symbol"
+edForm =  "define: missing form"
+edCls  =  "define: missing close curly"
+elPL   =  "lambda: missing parameter list"
+elPrms =  "lambda: duplicate parameter names"
+elPCls =  "lambda: missing parameter list close curly"
+elBody =  "lambda: missing body form"
+elCls  =  "lambda: missing close curly"
+esName =  "special form: unable to parse"
+ewUnp  =  "woof: unparsed input"
+-- other possibilities:  non-symbol in parameter list
 
--- tstring :: (MonadState [Token] m, Plus m, Switch m) => m AST
-tstring = fmap AString string
-
--- tnumber :: (MonadState [Token] m, Plus m, Switch m) => m AST
-tnumber = fmap (ANumber . read) number
-
--- application :: (MonadState [Token] m, Plus m, Switch m) => m AST
 application =
-    top            >>= \open ->
-    operator open  >>= \op ->
-    many0 form     >>= \args ->
-    close open     >>
+    oparen                       >>= \open ->
+    commit (eaOp, open) form     >>= \op ->
+    many0 form                   >>= \args ->
+    commit (eaCls, open) cparen  >>
     return (AApp op args)
-  where
-    operator open = commit ("missing application operator", open) form
-    close open = commit ("missing application close", open) tcp
     
-lambda =
-    check (== "lambda") symbol  >>
-    toc                         >>= \open ->
-    pars open                   >>= \params ->
-    close open                  >>
-    body open                   >>= \bodies ->
-    return (ALambda params bodies)
-  where
-    pars open = 
-        many0 symbol >>= \them -> 
-        if distinct them then return them
-                         else throwError ("duplicate parameter names", open)
-    distinct names = length names == length (nub names)
-    close open = commit ("missing parameter list close", open) tcc
-    body open = commit ("missing 1 or more lambda bodies", open) (many1 form)
-
-define open =
+define =
+    ocurly                       >>= \open ->
     check (== "define") symbol    *>
     pure ADefine                 <*>
-    sym                          <*>
-    for
-  where
-    sym = commit ("define: missing symbol", open) symbol
-    for = commit ("define: missing form", open) form
+    commit (edSym, open) symbol  <*>
+    commit (edForm, open) form   <*
+    commit (edCls, open) ccurly  
     
-special = 
-    toc          >>= \open ->
-    spec open    >>= \val ->
-    close open   >>
-    return val
+lambda =
+    ocurly                                 >>= \open ->
+    check (== "lambda") symbol             >>
+    commit (elPL, open) ocurly             >>= \p_open ->
+    many0 symbol                           >>= \params ->
+    (if distinct params 
+        then return ()
+        else throwError (elPrms, p_open))  >>
+    commit (elPCls, p_open) ccurly         >>
+    commit (elBody, open) (many1 form)     >>= \bodies ->
+    commit (elCls, open) ccurly            >>
+    return (ALambda params bodies)
   where
-    spec open = commit ("unable to parse special form", open) (lambda <+> define open)
-    close open = commit ("missing special form close", open) tcc
-    
-form = foldr (<+>) zero [tsymbol, tnumber, tstring, application, special]
+    distinct names = length names == length (nub names)
 
-parser = many0 form <* endCheck
-  where
-    endCheck = 
-        many0 (whitespace <+> comment) *>
-        get >>= \xs -> case xs of
-                            (t:_) -> throwError ("unparsed input", t)
-                            []    -> pure ()
+special = define <+> lambda <+> (ocurly >>= \o -> throwError (esName, o))
 
-runParse :: Parse s e t a -> [t] -> s -> Either' e (Maybe ((a, [t]), s))
-runParse p xs s = runMaybeT (runStateT (runStateT p xs) s)
+form = fmap ASymbol symbol <+> application <+> special
+
+endCheck = 
+    many0 (whitespace <+> comment) *>
+    get >>= \xs -> case xs of
+                        (t:_) -> throwError (ewUnp, t)
+                        []    -> pure ()
+
+woof = many0 form <* endCheck
+
 
 type Parse s e t a = StateT [t] (StateT s (MaybeT (Either' e))) a
+
+runParser :: Parse s e t a -> [t] -> s -> Either' e (Maybe ((a, [t]), s))
+runParser p xs s = runMaybeT (runStateT (runStateT p xs) s)
