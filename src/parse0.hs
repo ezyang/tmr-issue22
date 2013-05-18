c0b28de94146f82d7207f9771c213e62464e6d1c
Matt Fenwick <mfenwick100@gmail.com>
2013-05-18 14:17:17 -0400

    extend intro parser:  nested () example, and also add explanation of it to article, and re-work explanation

--- src/parse0.hs
+++ src/parse0.hs
@@ -34,3 +34,21 @@ check p m =
 
 literal :: Eq t => t -> Parser t t
 literal x = check (== x) item
+
+
+data Nesting
+    = One Char
+    | Many [Nesting]
+  deriving (Show, Eq)
+  
+char :: Parser Char Nesting
+char = fmap One $ check (not . flip elem "()") item
+
+level :: Parser Char Nesting
+level = literal '(' *> (fmap Many $ many element) <* literal ')'
+
+element :: Parser Char Nesting
+element = char <|> level
+
+parseNest :: Parser Char [Nesting]
+parseNest = many element
