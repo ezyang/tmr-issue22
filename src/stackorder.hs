7bb9dcbebfa8b643b873be1070535e23dfa7dcf2
Matt Fenwick <mfenwick100@gmail.com>
2013-06-10 12:02:08 -0400

    example code showing that ErrorT/MaybeT transformer order doesn't matter

--- src/stackorder.hs
+++ src/stackorder.hs
@@ -29,3 +29,23 @@ satisfy = flip check item
 
 literal :: (Eq t, MonadState [t] m, Alternative m) => t -> m t
 literal c = satisfy ((==) c)
+
+
+type Stack1 e a = Either e (Maybe a)
+type Stack2 e a = Maybe (Either e a)
+
+forward :: Stack1 e a -> Stack2 e a
+forward (Left e)          =  Just $ Left e
+forward (Right Nothing)   =  Nothing
+forward (Right (Just x))  =  Just $ Right x
+
+backward :: Stack2 e a -> Stack1 e a
+backward Nothing           =  Right Nothing
+backward (Just (Left e))   =  Left e
+backward (Just (Right x))  =  Right $ Just x
+
+identityForward :: Stack1 e a -> Stack1 e a
+identityForward = backward . forward
+
+identityBackward :: Stack2 e a -> Stack2 e a
+identityBackward = forward . backward
