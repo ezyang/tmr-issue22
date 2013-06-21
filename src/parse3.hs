63c2573afe0182d2c32fe648184587e696826244
Matt Fenwick <mfenwick100@gmail.com>
2013-06-21 15:17:27 -0400

    Parser source: count line/column using an extra state layer instead of pre-processing input; split the error-reporting section in 2: one for error messages, the second for position as well

--- src/parse3.hs
+++ src/parse3.hs
@@ -10,31 +10,54 @@ import Control.Monad.Trans.Maybe (MaybeT(..))
 import Control.Applicative       (Applicative(..), Alternative(..))
 import Data.List                 (nub)
 
+
+-- parser definition
+
+type Pos = (Int, Int)
+type Err = (String, Pos)
+
+type Parser t a = StateT [t] (StateT Pos (MaybeT (Either Err))) a 
+
+runParser :: Parser t a -> [t] -> Either Err (Maybe ((a, [t]), Pos))
+runParser p ts = runMaybeT $ runStateT (runStateT p ts) (1, 1)
+
+-- basic parser:  item
+
+getState :: Parser Char Pos
+getState = lift get
+
+putState :: Pos -> Parser Char ()
+putState = lift . put
+
+updateState :: (Pos -> Pos) -> Parser Char ()
+updateState f = getState >>= (putState . f)
+
+basic_item :: (MonadState [t] m, Alternative m) => m t
+basic_item =
+    get >>= \xs -> case xs of
+                        (t:ts) -> put ts *> pure t;
+                        []     -> empty;
+
+item :: Parser Char Char
+item = basic_item >>= \x -> updateState (f x) >> pure x
+  where f '\n' (ln, c) = (ln + 1, 1)
+        f _    (ln, c) = (ln, c + 1)
+
 -- the prelude
-
-type Parser e t a = StateT [t] (MaybeT (Either e)) a
-
-runParser :: Parser e t a -> [t] -> Either e (Maybe (a, [t]))
-runParser p xs = runMaybeT (runStateT p xs)
 
 many0 = many
 many1 = some
 
-item :: (MonadState [t] m, Alternative m) => m t
-item =
-    get >>= \xs -> case xs of
-                        (t:ts) -> put ts *> pure t;
-                        []     -> empty;
 
 check :: (Monad m, Alternative m) => (a -> Bool) -> m a -> m a
 check f p =
     p >>= \x ->
     if (f x) then return x else empty
 
-satisfy :: (MonadState [t] m, Alternative m) => (t -> Bool) -> m t
+satisfy :: (Char -> Bool) -> Parser Char Char
 satisfy = flip check item
 
-literal :: (Eq t, MonadState [t] m, Alternative m) => t -> m t
+literal :: Char -> Parser Char Char
 literal c = satisfy ((==) c)
 
 
@@ -51,11 +74,8 @@ instance (Functor m, Switch m) => Switch (StateT s m) where
 instance Functor m => Switch (MaybeT m) where
   switch (MaybeT m) = MaybeT (fmap switch m)
 
-not1 :: (MonadState [t] m, Alternative m, Switch m) => m a -> m t
+not1 :: Parser Char a -> Parser Char Char
 not1 p = switch p *> item
-
-end :: (MonadState [t] m, Alternative m, Switch m) => m ()
-end = switch item
 
 
 class Monad m => MonadError e m | m -> e where
@@ -81,6 +101,7 @@ instance MonadError e m => MonadError e (MaybeT m) where
 commit :: (MonadError e m, Alternative m) => e -> m a -> m a
 commit err p = p <|> throwError err
 
+-- 
 
 data AST
     = ASymbol String
@@ -103,20 +124,18 @@ example = "{define \n\
 
 -- the actual parser
 
-
-type Error = String
-
-
 whitespace = many1 $ satisfy (flip elem " \n\t\r\f")
 comment = pure (:) <*> literal ';' <*> many0 (not1 $ literal '\n')
 
-munch p = many0 (whitespace <|> comment) *> p
+junk = many0 (whitespace <|> comment)
 
-opencurly  = munch $ literal '{'
-closecurly = munch $ literal '}'
-openparen  = munch $ literal '('
-closeparen = munch $ literal ')'
-symbol = munch $ many1 char
+tok p = p <* junk
+
+opencurly  = tok $ literal '{'
+closecurly = tok $ literal '}'
+openparen  = tok $ literal '('
+closeparen = tok $ literal ')'
+symbol = tok $ many1 char
   where char = satisfy (flip elem (['a' .. 'z'] ++ ['A' .. 'Z']))
 
 eAppOper    =  "application: missing operator"
@@ -134,41 +153,51 @@ eWoof       =  "woof: unparsed input"
 -- other possibilities:  non-symbol in parameter list
 
 application =
-    openparen                     >>
-    commit eAppOper form          >>= \op ->
-    many0 form                    >>= \args ->
-    commit eAppClose closeparen   >>
+    openparen                           >>
+    getState                            >>= \p1 -> 
+    commit (eAppOper, p1) form          >>= \op ->
+    many0 form                          >>= \args ->
+    getState                            >>= \p2 ->
+    commit (eAppClose, p2) closeparen   >>
     return (AApp op args)
     
 define =
-    check (== "define") symbol    *>
-    pure ADefine                 <*>
-    commit eDefSym symbol        <*>
-    commit eDefForm form         <*
-    commit eDefClose closecurly
+    check (== "define") symbol          >>
+    getState                            >>= \p1 ->
+    commit (eDefSym, p1) symbol         >>= \s  ->
+    getState                            >>= \p2 ->
+    commit (eDefForm, p2) form          >>= \f  ->
+    getState                            >>= \p3 ->
+    commit (eDefClose, p3) closecurly   >>
+    return (ADefine s f)
     
 lambda =
-    check (== "lambda") symbol       >>
-    commit eLamParam opencurly       >>
-    many0 symbol                     >>= \params ->
+    check (== "lambda") symbol             >>
+    getState                               >>= \p1 ->
+    commit (eLamParam, p1) opencurly       >>
+    getState                               >>= \p2 ->
+    many0 symbol                           >>= \params ->
     (if distinct params 
         then return ()
-        else throwError eLamDupe)    >>
-    commit eLamPClose closecurly     >>
-    commit eLamBody (many1 form)     >>= \bodies ->
-    return (ALambda params bodies)   <*
-    commit eLamClose closecurly  
+        else throwError (eLamDupe, p2))    >>
+    getState                               >>= \p3 ->
+    commit (eLamPClose, p3) closecurly     >>
+    getState                               >>= \p4 ->
+    commit (eLamBody, p4) (many1 form)     >>= \bodies ->
+    getState                               >>= \p5 ->
+    commit (eLamClose, p5) closecurly      >>
+    return (ALambda params bodies)
   where
     distinct names = length names == length (nub names)
 
 special = 
-    opencurly  *>  (define <|> lambda <|> throwError eSpecial)
+    opencurly                       *>
+    getState                       >>= \p1 ->  
+    commit (eSpecial, p1) spForm
+  where spForm = define <|> lambda
 
 form = fmap ASymbol symbol <|> application <|> special
 
-endCheck = 
-    many0 (whitespace <|> comment) *>
-    switch item
+endCheck = switch item
 
-woof :: Parser String Char [AST]
-woof = many0 form <* commit eWoof endCheck
+woof = junk *> many0 form <* (getState >>= \p1 -> commit (eWoof, p1) endCheck)
